b5b71046094fe0fd04d8543dfc2d8f7218a4031e
Ankit Kushwaha <ankit26kushwa@gmail.com>
2023-04-11 19:23:09 +0530

    fix eslint error

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-let union = document.querySelector(".union");
-let navbar = document.querySelector(".top_navbar");
-let logo = document.querySelector("#logo");
-let main = document.querySelector(".main-section");
+const union = document.querySelector('.union');
+const navbar = document.querySelector('.top_navbar');
+const logo = document.querySelector('#logo');
+const main = document.querySelector('.main-section');
 
-union.addEventListener("click", function () {
+union.addEventListener('click', () => {
   // alert("javascript working");
 
-  let cross = document.createElement("li");
-  cross.id = "cross";
-  cross.innerHTML = "×";
+  const cross = document.createElement('li');
+  cross.id = 'cross';
+  cross.innerHTML = '×';
 
-  navbar.classList.add("mobile-menu");
-  logo.classList.add("mobile-back");
-  main.classList.add("mobile-back");
-  union.classList.add("img_remove");
+  navbar.classList.add('mobile-menu');
+  logo.classList.add('mobile-back');
+  main.classList.add('mobile-back');
+  union.classList.add('img_remove');
   navbar.prepend(cross);
 
-  let cross2 = document.querySelector("#cross");
-  cross2.addEventListener("click", function () {
+  const cross2 = document.querySelector('#cross');
+  cross2.addEventListener('click', () => {
     // alert("go for it");
-    navbar.classList.remove("mobile-menu");
-    logo.classList.remove("mobile-back");
-    main.classList.remove("mobile-back");
-    union.classList.remove("img_remove");
+    navbar.classList.remove('mobile-menu');
+    logo.classList.remove('mobile-back');
+    main.classList.remove('mobile-back');
+    union.classList.remove('img_remove');
     cross2.remove();
-  });
-
+  });
 });
-
-
